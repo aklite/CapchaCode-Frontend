73fa4f42a1bf8aa616ab41ec96ff9999b9a8c60a
AnkanSaha <ankansahadaluabari@gmail.com>
2023-03-20 20:01:42 +0530

    Modified: #421 Edited Update_title Function

--- src/API/Effect/Update Document Title.jsx
+++ src/API/Effect/Update Document Title.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect } from "react";
 
-function Update_Document_Title(Title) {
+export default function Update_Document_Title(Title) {
   // Title is a string
   useEffect(() => {
     document.title = Title;
   }, []);
 } // End of Update_Document_Title
-
-export default Update_Document_Title; // Exporting the function
